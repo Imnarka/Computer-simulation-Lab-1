a164c8a8f5ad8fd33a5a2b6cae7b14ae009da1b6
Linar Fazlykhakov <67334757+Imnarka@users.noreply.github.com>
2021-10-04 23:49:28 +0500

    Update lab1.m

--- lab1.m
+++ lab1.m
@@ -1,10 +1,10 @@
 a = 0.11; b = 0.09;
 L = 0.5 * sqrt(b^2 + 16 * a^2) + (b^2) * log((4 * a + sqrt(b^2 + 16 * a^2))/b)/(8 * a)
 %----------------------------------------------------
-%--Формула для изменения основания логарифма: log a(N) = log b(N)/log b(a)
-%--Пункт а)
+%--The formula for changing the base of the logarithm: log a(N) = log b(N)/log b(a)
+%-Task a)
 %--log 4(0.085) =
 anwser = log(0.085)/log(4)
-%--Пункт б)
+%--Task b)
 %--log 6(1500) =
 answer = log10(1500)/log10(10)
