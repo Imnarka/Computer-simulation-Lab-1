c020ac81d3d0ba5f2378d219e505c24cea56dbc3
Linar Fazlykhakov <67334757+Imnarka@users.noreply.github.com>
2021-10-04 23:12:35 +0500

    Update lab1.m

--- lab1.m
+++ lab1.m
@@ -1,6 +1,6 @@
 a = 0.11; b = 0.09;
 L = 0.5 * sqrt(b^2 + 16 * a^2) + (b^2) * log((4 * a + sqrt(b^2 + 16 * a^2))/b)/(8 * a)
 %----------------------------------------------------
-%--Формула для изменения основания логарифма: log a(N) = log b(N)/log b(a)
+%--The formula for changing the base of the logarithm: log a(N) = log b(N)/log b(a)
 %--log 4(0.085) =
 anwser = log(0.085)/log(4)
